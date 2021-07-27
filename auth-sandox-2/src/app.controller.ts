7c44fa858b75a5934e84b3b2190c1e4c27599a7f
Nicolas Essayan <nicolas_essayan@yahoo.fr>
2021-07-27 16:52:38 +0200

    route protected

--- auth-sandox-2/src/app.controller.ts
+++ auth-sandox-2/src/app.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { JwtAuthenticationGuard } from './authentication/jwtAuthentication.guard';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @UseGuards(JwtAuthenticationGuard)
   @Get()
   getHello(): string {
     return this.appService.getHello();
