60bfca3cdb01d7b80f18eb5756b50d32d36bdb0b
clbrunet <brunet.clement5@gmail.com>
2021-09-07 18:21:30 +0200

    feat(back): move 42 API secrets to .env

--- back-end-nest/src/authentication/fortyTwo.strategy.ts
+++ back-end-nest/src/authentication/fortyTwo.strategy.ts
@@ -10,8 +10,8 @@ export class FortyTwoStrategy extends PassportStrategy(Strategy, 'fortyTwo') {
     super({
       authorizationURL: 'https://api.intra.42.fr/oauth/authorize',
       tokenURL: 'https://api.intra.42.fr/oauth/token',
-      clientID: '9bf776aebb6591e065d48ddfcc3d16da20f4390dc25be24084702d9560132e06',
-      clientSecret: 'affd3a319e0abc6f0df8eb643a1524bbed9ddc481fb211952d2e4aa49fc5980f',
+      clientID: process.env.CLIENT_ID_42,
+      clientSecret: process.env.CLIENT_SECRET_42,
       callbackURL: `${ process.env.FRONT_URL }/oauth-forty-two`,
     });
   }
