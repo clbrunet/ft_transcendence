423edabb57f32a8a50e6f57a79ed0a834e87719d
clbrunet <brunet.clement5@gmail.com>
2021-09-28 08:15:59 +0200

    feat(front): basic admin view

--- front-end-vue/src/views/Admin.vue
+++ front-end-vue/src/views/Admin.vue
@@ -1,134 +1,25 @@
 <template>
   <div id="body">
-      <div class="settings">
-        <h3>Duel's settings :</h3>
-        <div class="settings-points">
-          <span class="nbPoints"> Number of points </span>
-          <select v-model="nbPointsConfig" class="selectDuel">
-            <option selected>3</option>
-            <option>4</option>
-            <option>5</option>
-            <option>6</option>
-            <option>7</option>
-            <option>8</option>
-            <option>9</option>
-            <option>10</option>
-          </select>
-        </div>
-      </div>
     <table>
       <tr style="background-color:#aaa;">
         <td style="text-align:center;">Username</td>
-        <td style="text-align:center;">Level</td>
-        <td style="text-align:center;">XP</td>
-        <td style="text-align:center;">Stats</td>
-        <td style="text-align:center;">Duel</td>
-        <td style="text-align:center;">Send DM</td>
-        <td style="text-align:center;">Friends</td>
-        <td style="text-align:center;">Blocks</td>
+        <td style="text-align:center;">Email</td>
+        <td style="text-align:center;">2FA</td>
+        <td style="text-align:center;">Avatar</td>
+        <td style="text-align:center;">Status</td>
+        <td style="text-align:center;">Admin</td>
+        <td style="text-align:center;">inQueue</td>
       </tr>
       <template v-for="(user, index) in users">
         <template v-if="user.id != $store.state.user.id">
           <tr :key="index">
             <td class="field clickable" @click="goToProfile(user)">{{user.name}}</td>
-            <td class="field">{{user.level}}</td>
-            <td class="field">{{user.xp}} xp</td>
-            <td class="field">{{user.nWins}}W / {{user.nLosses}}L</td>
-            <template v-if="tabBlocks[index] == 'blocker' || tabBlocks[index] == 'blocked'">
-              <td class="field">Blocked</td>
-              <td class="field">Blocked</td>
-              <td class="field">Blocked</td>
-              <td v-if="tabBlocks[index] == 'blocker'" class="field btnBox">
-                <img
-                  src="/assets/unblock.svg"
-                  alt="unblock"
-                  style="width:30px;cursor:pointer;"
-                  @click="unblock(user)"
-                />
-              </td>
-              <td v-else class="field">Blocked</td>
-            </template>
-            <template v-else-if="friends">
-              <td class="field btnBox">
-                <img
-                  v-if="tabDuels != undefined && tabDuels[index] != undefined && tabDuels[index].status == 'none'"        
-                  src="/assets/duel.svg"
-                  alt="duel"
-                  style="width:30px;cursor:pointer;"
-                  @click="duel(user)"
-                />
-                <button class="btn-ok" v-else-if="tabDuels != undefined && tabDuels[index] != undefined && tabDuels[index].status == 'sent'" @click="unduel(user)">Unduel</button>
-                <template v-else-if="tabDuels != undefined && tabDuels[index] != undefined && tabDuels[index].status == 'received'">
-                  <img
-                    src="/assets/accept-button.svg"
-                    alt="accept-button"
-                    style="width:20px;cursor:pointer;margin-right:5px;"
-                    @click="accept_duel(user, tabDuels[index].id)"
-                  />
-                  <img
-                    src="/assets/deny-button.svg"
-                    alt="deny-button"
-                    style="width:20px;cursor:pointer;margin-left:5px;"
-                    @click="deny_duel(user)"
-                  />
-                </template>
-                <img
-                  v-else        
-                  src="/assets/duel.svg"
-                  alt="duel"
-                  style="width:30px;cursor:pointer;"
-                  @click="duel(user)"
-                />
-              </td>
-              <td class="field btnBox">
-                <img
-                  src="/assets/direct-message.svg"
-                  alt="send dm"
-                  style="width:30px;cursor:pointer;"
-                  @click="send_message(user)"
-                />
-              </td>
-              <td class="field btnBox">
-                <template v-if="tab[index] == 'received'">
-                  <img
-                    src="/assets/accept-button.svg"
-                    alt="accept-button"
-                    style="width:20px;cursor:pointer;margin-right:5px;"
-                    @click="accept_friend_request(user)"
-                  />
-                  <img
-                    src="/assets/deny-button.svg"
-                    alt="deny-button"
-                    style="width:20px;cursor:pointer;margin-left:5px;"
-                    @click="deny_friend_request(user)"
-                  />
-
-                </template>
-                <span class="field" v-else-if="tab[index] == 'sent'">Sent</span>
-                <img
-                  v-else-if="tab[index] == 'accepted'"
-                  src="/assets/remove-friend.svg"
-                  alt="remove-friend"
-                  style="width:30px;cursor:pointer;"
-                  @click="remove_friend(user)"
-                />
-                <img
-                  v-else
-                  src="/assets/add-friend.svg"
-                  alt="add-friend"
-                  style="width:30px;cursor:pointer;"
-                  @click="add_friend(user)"
-                />
-              </td>
-              <td class="field btnBox">
-                <img
-                  src="/assets/block.svg"
-                  alt="block"
-                  style="width:30px;cursor:pointer;"
-                  @click="block(user)"
-                />
-              </td>
-            </template>
+            <td class="field">{{user.email}}</td>
+            <td class="field">{{user.isTwoFactorAuthenticationEnabled}}</td>
+            <td class="field"><a v-bind:href="user.avatar" target="_blank">{{user.avatar}}</a></td>
+            <td class="field">{{user.status}}</td>
+            <td class="field">{{user.admin}}</td>
+            <td class="field">{{user.inQueue}}</td>
           </tr>
         </template>
       </template>
@@ -140,22 +31,18 @@
 /* eslint-disable */
 import Vue from "vue";
 import axios from "axios";
-import io from "socket.io-client"
 import router from "../router";
+import Store from "../store";
 
 export default Vue.extend({
   name: "Admin",
+  store: Store,
   data() {
     return {
       users: {} as any,
       blocks: {} as any,
       friends: {} as any,
       tab: [] as any,
-      socket: {} as any,
-      tabBlocks: [] as any,
-      duels: [] as any,
-      tabDuels: undefined as any,
-      nbPointsConfig: 3 as any
     };
   },
   mounted() {
@@ -165,116 +52,23 @@ export default Vue.extend({
 
 
     this.get_users();
-    this.get_blocks();
-    this.get_duels();
-
-    this.$store.state.socket.on('refreshDuels', (id: any) => {
-      if (this.$store.state.user != undefined)
-      {
-        if (this.$store.state.user.id == id)
-        {
-          this.get_duels();
-        }
-      }
-    });
 
     this.$store.state.socket.on('refreshAllUsers', (idUser: any) => {
       if (this.$store.state.user.id == idUser || idUser == 'all')
       {
         this.get_users();
       }
-    })
-
-    /* check game ongoing */
-    if (this.$store.state.user != undefined)
-    {
-      axios({
-        url: process.env.VUE_APP_API_URL + "/game/indexOngoing",
-        method: "get",
-        withCredentials: true
-      }).then(res => {
-        let allOngoing = res.data;
-
-        for (let i = 0; i < allOngoing.length; i++)
-        {
-          if (allOngoing[i].players[0].userId == this.$store.state.user.id || allOngoing[i].players[1].userId == this.$store.state.user.id)
-          {
-            axios({
-              url: process.env.VUE_APP_API_URL + "/game/unfinished/" + allOngoing[i].id,
-              method: "patch",
-              withCredentials: true
-            }).then(() => {
-              this.$store.state.socket.emit('gameBugged', {idGame:allOngoing[i].id, page:'Users', idUser: this.$store.state.user.id});
-            }).catch(() => {
-              alert("There was an error, back to profile")
-              router.push({name: 'Profile'});
-            });
-          }
-        }
-      }).catch(() => {
-        alert("There was an error, back to profile")
-        router.push({name: 'Profile'});
-      });
-    }
-    /**/
+    });
   },
   methods: {
     get_users() {
       axios({
-        url: `${process.env.VUE_APP_API_URL}/user/index`,
+        url: `${process.env.VUE_APP_API_URL}/user/all`,
         method: "get",
         withCredentials: true
       }).then(res => {
         this.users = res.data;
         this.get_friends();
-        this.get_blocks();
-      });
-    },
-    get_blocks() {
-      axios({
-        url: `${process.env.VUE_APP_API_URL}/block/index`,
-        method: "get",
-        withCredentials: true
-      }).then(res => {
-        this.blocks = res.data;
-        this.tabBlocks.length = 0;
-        for (let i = 0; i < this.users.length; i++) {
-          var flag = false;
-          if (this.users[i].id != this.$store.state.user.id)
-          {
-            var flag = false;
-            for (let j = 0; j < this.blocks.length; j++) {
-              if (this.users[i].id == this.blocks[j].blockId) {
-                this.tabBlocks.push(this.blocks[j].status);
-                flag = true;
-              }
-            }
-          }
-          if (flag == false) this.tabBlocks.push("none");
-        }
-      });
-    },
-    get_duels() {
-      axios({
-        url: `${process.env.VUE_APP_API_URL}/duel/index`,
-        method: "get",
-        withCredentials: true
-      }).then(res => {
-        this.duels = res.data;
-        if (this.tabDuels != undefined)
-          this.tabDuels.length = 0;
-        else
-          this.tabDuels = [];
-        for (let i = 0; i < this.users.length; i++) {
-          var flag = false;
-          for (let j = 0; j < this.duels.length; j++) {
-            if (this.users[i].id == this.duels[j].duelId) {
-              this.tabDuels.push({status:this.duels[j].status, id: this.duels[j].id});
-              flag = true;
-            }
-          }
-          if (flag == false) this.tabDuels.push({status: "none"});
-        }
       });
     },
     get_friends() {
@@ -295,99 +89,6 @@ export default Vue.extend({
           }
           if (flag == false) this.tab.push("none");
         }
-      });
-    },
-    duel(user: any) {
-      const url = `${process.env.VUE_APP_API_URL}/duel/` + user.id;
-      axios({
-        url: url,
-        method: "post",
-        withCredentials: true
-      }).then(res => {
-        this.get_users();
-        this.get_duels();
-        axios({
-          url: `${process.env.VUE_APP_API_URL}/direct/go/` + user.id,
-          method: "get",
-          withCredentials: true
-        }).then(res => {
-          let newId;
-          if (res.data.participants[0].userId != this.$store.state.user.id) {
-            newId = res.data.participants[0].userId;
-          }
-          else {
-            newId = res.data.participants[1].userId;
-          }
-          
-          this.$store.state.socket.emit('duelSent', {idRoom: res.data.id, id: newId});
-          this.$store.state.socket.emit('duelDenied', {idRoom: "room", id: user.id})
-          axios({
-            url: `${process.env.VUE_APP_API_URL}/duel/go/` + user.id,
-            method: "post",
-            withCredentials: true 
-          }).then((res) => {
-            if (res.data == 'Duel cancelled since the active User is already in-game')
-            {
-              alert('you were already in a game, you can now play');
-              axios({
-                    url: process.env.VUE_APP_API_URL + "/game/indexOngoing",
-                    method: "get",
-                    withCredentials: true
-                  }).then(res => {
-                    let allOngoing = res.data;
-
-                    for (let i = 0; i < allOngoing.length; i++)
-                    {
-                      if (allOngoing[i].players[0].userId == this.$store.state.user.id || allOngoing[i].players[1].userId == this.$store.state.user.id)
-                      {
-                        axios({
-                          url: process.env.VUE_APP_API_URL + "/game/unfinished/" + allOngoing[i].id,
-                          method: "patch",
-                          withCredentials: true
-                        }).then(() => {
-                          this.$store.state.socket.emit('gameBugged', {idGame:allOngoing[i].id, page:'Profile', idUser: this.$store.state.user.id});
-                        }).catch(() => {
-                          alert("There was an error, back to profile")
-                          router.push({name: 'Profile'});
-                        });
-                      }
-                    }
-                  }).catch(() => console.log(""));
-            }
-            else if (this.$store.state.user.id != undefined)
-            {
-              this.$store.state.gameid = res.data.id;
-              this.$store.state.nbPoints = this.nbPointsConfig;
-            }
-          });
-        });
-      }).catch(() => console.log(''));
-    },
-    unduel(user: any) {
-      const url = `${process.env.VUE_APP_API_URL}/duel/unduel/` + user.id;
-      axios({
-        url: url,
-        method: "delete",
-        withCredentials: true
-      }).then(res => {
-        this.get_users();
-        this.get_duels();
-
-        axios({
-          url: `${process.env.VUE_APP_API_URL}/direct/go/` + user.id,
-          method: "get",
-          withCredentials: true
-        }).then(res => {
-          let newId;
-          if (res.data.participants[0].userId != this.$store.state.user.id) {
-            newId = res.data.participants[0].userId;
-          }
-          else {
-            newId = res.data.participants[1].userId;
-          }
-          this.$store.state.socket.emit('duelDenied', {idRoom: "room", id: user.id})
-          this.$store.state.socket.emit('duelSent', {idRoom: res.data.id, id: newId})
-        });
       });
     },
     add_friend(user: any) {
@@ -446,7 +147,6 @@ export default Vue.extend({
         withCredentials: true
       }).then(() => {
         this.get_users();
-        this.get_blocks();
         this.$store.state.socket.emit('refreshChannels');
         this.$store.state.socket.emit('refreshUsers', user.id);
       });
@@ -459,7 +159,6 @@ export default Vue.extend({
         withCredentials: true
       }).then(() => {
         this.get_users();
-        this.get_blocks();
         this.$store.state.socket.emit('refreshChannels');
         this.$store.state.socket.emit('refreshUsers', user.id);
       });
@@ -479,43 +178,6 @@ export default Vue.extend({
       const path = "/profile/" + user.id;
       router.push({ path: path });
     },
-    accept_duel(user: any, duelId: any) {
-      axios({
-        url: `${ process.env.VUE_APP_API_URL }/duel/accept/` + user.id,
-        method: "patch",
-        withCredentials: true
-      }).then(res => {
-        if (res.data == 'Duel cancelled since the other User is offline' ||
-            res.data == 'Duel cancelled since at least one of the User is already in-game')
-        {
-          this.$store.state.socket.emit('duelDenied', {idRoom: 'room', id: user.id})
-          alert(res.data);
-          this.get_duels();
-        }
-        else
-        {
-          this.get_duels();
-          this.$store.state.gameid2 = res.data.id;
-          this.$store.state.socket.emit('duelAccepted', {idRoom: "room", id: user.id, ownerId: this.$store.state.user.id, duelId: res.data.id})
-        }
-        this.get_duels();
-      }).catch(err => {
-        alert('Duel cancelled because of other user disconnected')
-        this.get_duels();
-      });
-    },
-    deny_duel(user: any) {
-      axios({
-        url: `${ process.env.VUE_APP_API_URL }/duel/reject/` + user.id,
-        method: "patch",
-        withCredentials: true
-      }).then(res => {
-        this.get_duels();
-        this.$store.state.socket.emit('duelDenied', {idRoom: "room", id: user.id})
-      }).catch(err => {
-        console.log("");
-      })
-    }
   }
 });
 </script>
@@ -543,10 +205,6 @@ h1 {
   height: 91vh;
   width: 100%;
   flex-direction: column;
-}
-
-.settings-points{
-  width:170px;
 }
 
 table {
@@ -641,27 +299,22 @@ tr {
   text-align:center;
 }
 
+.field a {
+  color: inherit;
+  text-decoration: none;
+}
+
+.field a:hover {
+  text-decoration: underline;
+}
+
 .btn {
   cursor: pointer;
   height: 100%;
   margin-left: auto;
 }
 
-.settings {
-  background-color:white;
-  display:flex;
-  align-items:center;
-  justify-content:center;
-  flex-direction: column;
-  padding:  0.5% 1.8% 0.5% 1.8%;
-  border-radius:15px;
-  border: 1px solid #3040F0;
-  color:white;
-  width: fit-content;
-}
-
 h3 {
   color: black;
 }
-
 </style>
